d67aa8c1220633c2d7e36eac32abb4a1b2b7eaab
Catherine Farman <Catherine.Farman@cfpb.gov>
2018-05-22 20:11:53 -0400

    add solutions to code exercises to link to from slides

---
+++ solutions/fortune-teller.js
@@ -0,0 +1,7 @@
+var jobTitle = 'web developer';
+var home = 'sprawling beachfront treehouse';
+var place = 'Costa Rica';
+var hobby = 'play Scrabble';
+
+var future = 'You will be a ' + jobTitle + ' living in a ' + home + ' in ' + place + '. For fun you will ' + hobby + '.'
+console.log(future);
